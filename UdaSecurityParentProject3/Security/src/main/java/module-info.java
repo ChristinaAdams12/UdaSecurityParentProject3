c886d90c41249848a1ccdb0d033adaa666596b19
Christina Adams <31103837+ChristinaAdams12@users.noreply.github.com>
2022-12-12 20:42:44 -0800

    Add files via upload

---
+++ UdaSecurityParentProject3/Security/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module Security {
+
+    requires Image;
+    requires com.miglayout.swing;
+    requires java.desktop;
+    requires java.prefs;
+    requires com.google.gson;
+    requires guava;
+    exports com.udacity.security.application;
+    exports com.udacity.security.data;
+    exports com.udacity.security.service;
+}
