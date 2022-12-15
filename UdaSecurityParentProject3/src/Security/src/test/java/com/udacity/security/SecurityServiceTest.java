1b61c8c0cadcf3166f15c0e10a871c5b00907a62
Christina Adams <31103837+ChristinaAdams12@users.noreply.github.com>
2022-12-14 18:50:04 -0800

    Add files via upload

---
+++ UdaSecurityParentProject3/src/Security/src/test/java/com/udacity/security/SecurityServiceTest.java
@@ -0,0 +1,43 @@
+package com.udacity.security;
+
+import com.udacity.image.interfaces.ImageService;
+import com.udacity.security.data.*;
+import com.udacity.security.service.SecurityService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class SecurityServiceTest {
+
+    @Mock
+    private ImageService imageService;
+
+    @Mock
+    private SecurityRepository securityRepository;
+
+    private SecurityService securityService;
+
+    @BeforeEach
+    void init() {
+        securityService = new SecurityService(securityRepository, imageService);
+    }
+
+    @Test
+    public void alarmArmed_and_sensorActivated_setSystemTo_pendingAlarmStatus() {
+
+        Sensor sensor = new Sensor("Front door", SensorType.DOOR);
+
+        securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
+        securityService.changeSensorActivationStatus(sensor, true);
+        securityService.setAlarmStatus(AlarmStatus.PENDING_ALARM);
+
+        verify(securityRepository).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+
+
+    }
+
+}
